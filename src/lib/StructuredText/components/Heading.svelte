4ad5d9e3f2612f8028ef74c653e5ab10e80bfc5b
Łukasz Wiśniewski <fr.luksus@gmail.com>
2021-10-13 21:45:26 +0000

    fix: remove tailwind from default components

--- src/lib/StructuredText/components/Heading.svelte
+++ src/lib/StructuredText/components/Heading.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
+	import {H1,H2,H3,H4,H5,H6} from './Heading' 
 	export let level: 1 | 2 | 3 | 4 | 5 | 6 = 1;
 	const sizeMap = [
-		'text-base',
-		'text-xl',
-		'text-2xl',
-		'text-3xl',
-		'text-4xl',
-		'text-5xl',
-		'text-6xl'
-	].reverse();
+		H1,
+		H2,
+		H3,
+		H4,
+		H5,
+		H6
+	]
 </script>
 
-<h1 class={sizeMap[level - 1]}>
+<svelte:component this={sizeMap[level - 1]}>
 	<slot />
-</h1>
+</svelte:component>
--- src/lib/StructuredText/components/Paragraph.svelte
+++ src/lib/StructuredText/components/Paragraph.svelte
@@ -1,3 +1,3 @@
-<p class="text-xl">
+<p>
 	<slot />
 </p>
