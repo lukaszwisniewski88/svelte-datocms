5fdce1664c48c47aaa59760d857ba8326862aa00
lukaszwisniewski88 <fr.luksus@gmail.com>
2022-01-18 21:11:14 +0100

    feat: structured text can import only whats needed

--- src/lib/StructuredText/components/Renderer.svelte
+++ src/lib/StructuredText/components/Renderer.svelte
@@ -1,50 +1,40 @@
-<script context="module" lang="ts">
-	import type { ComponentsMap } from '../types';
-	import Root from './Root.svelte';
-	import Span from './Span.svelte';
-	import {Heading} from './Heading';
-	import Paragraph from './Paragraph.svelte';
-	import List from './List.svelte';
-	import Blockquote from './Blockquote.svelte';
-	import Emphasis from './Emphasis.svelte';
-	import Highlight from './Highlight.svelte';
-	import Underline from './Underline.svelte';
-	import Strong from './Strong.svelte';
-	import Thematicbreak from './Themeaticbreak.svelte';
-	import Strikethrough from './Strikethrough.svelte';
-	import ListItem from './Listitem.svelte';
-	import Link from './Link.svelte';
-	import Code from './Code.svelte';
-
-	const defaultMapping: ComponentsMap = {
-		root: Root,
-		span: Span,
-		heading: Heading,
-		list: List,
-		blockquote: Blockquote,
-		paragraph: Paragraph,
-		emphasis: Emphasis,
-		highlight: Highlight,
-		strikethrough: Strikethrough,
-		underline: Underline,
-		strong: Strong,
-		thematicBreak: Thematicbreak,
-		link: Link,
-		listItem: ListItem,
-		code: Code
-	};
-</script>
-
 <script lang="ts">
 	import type { NodeType, Mark } from 'datocms-structured-text-utils';
 	import { getContext as getComponentsContext } from '../components.context';
 	export let type: NodeType | Mark;
 	const components = getComponentsContext();
-	$: component = $components[type] ? $components[type] : defaultMapping[type];
+	$: component = new Promise(async (resolve, reject) => {
+		const defaultMapping = {
+			root: 'Root',
+			span: 'Span',
+			heading: 'Heading/Heading',
+			list: 'List',
+			blockquote: 'Blockquote',
+			paragraph: 'Paragraph',
+			emphasis: 'Emphasis',
+			highlight: 'Highlight',
+			strikethrough: 'Strikethrough',
+			underline: 'Underline',
+			strong: 'Strong',
+			thematicBreak: 'Themeaticbreak',
+			link: 'Link',
+			listItem: 'Listitem',
+			code: 'Code'
+		};
+		if ($components[type]) resolve($components[type]);
+		else {
+			/* @vite-ignore */
+			const result = await import(`./${defaultMapping[type]}.svelte`);
+			if (!result.default) reject('no components to render');
+			resolve(result.default);
+		}
+	});
 </script>
 
-{#if component}
-	<svelte:component this={component} {...$$restProps}>
+{#await component then render}
+	<svelte:component this={render} {...$$restProps}>
 		<slot />
 	</svelte:component>
-{/if}
+{:catch reason}
+	<div>{reason}</div>
+{/await}
--- src/routes/subscribe/index.svelte
+++ src/routes/subscribe/index.svelte
@@ -3,8 +3,8 @@
 	import type { QueryResponseType, QueryVariables } from './_query';
 	import { query } from './_query';
 	let enabled = true;
-	let pageSize = 10
-	let pageSizeTemp = 10
+	let pageSize = 10;
+	let pageSizeTemp = 10;
 	const statusMessage = {
 		connecting: 'Connecting to DatoCMS...',
 		connected: 'Connected to DatoCMS, receiving live updates!',
@@ -19,20 +19,22 @@
 	$: ({ data, error, status } = $store);
 </script>
 
-<main class="container px-8 mx-auto">
-	<h1 class="text-5xl font-semibold">Dato CMS Blog</h1>
+<main class="container mx-auto px-8">
+	<h1 class="font-semibold text-5xl">Dato CMS Blog</h1>
 	<h2 class="text-2xl">News, tips, highlights, and other updates from the team at DatoCMS.</h2>
-	<div class="flex flex-row gap-6 m-4 place-items-center">
+	<div class="flex flex-row m-4 gap-6 place-items-center">
 		{#if enabled}
-		<div class="rounded-full w-[12px] h-[12px] bg-green-400 relative">
-		<div class="rounded-full w-[12px] h-[12px] border-green-400/50 shadow-md border animate-heart-beat animate-loop absolute animate-duration-1000"/>
-		</div>
+			<div class="rounded-full bg-green-400 h-[12px] w-[12px] relative">
+				<div
+					class="border rounded-full border-green-400/50 h-[12px] shadow-md animate-heart-beat animate-loop w-[12px] animate-duration-1000 absolute"
+				/>
+			</div>
 		{:else}
-		<div class="rounded-full w-[12px] h-[12px] bg-red-400"/>
+			<div class="rounded-full bg-red-400 h-[12px] w-[12px]" />
 		{/if}
 		<diu class="font-mono text sm "> Status : {statusMessage[status]}</diu>
 		<button
-			class="py-2 px-4 rounded bg-gray-500 text-white inline-flex place-items-center"
+			class="rounded bg-gray-500 text-white py-2 px-4 inline-flex place-items-center"
 			on:click={() => (enabled = !enabled)}>{enabled ? 'Disconnect' : 'Connect'}</button
 		>
 	</div>
@@ -40,7 +42,13 @@
 		<span>
 			Page size : {pageSizeTemp}
 		</span>
-		<input type=range min=1 max=25 bind:value={pageSizeTemp} on:mouseup={()=>pageSize = pageSizeTemp}/>
+		<input
+			type="range"
+			min="1"
+			max="25"
+			bind:value={pageSizeTemp}
+			on:mouseup={() => (pageSize = pageSizeTemp)}
+		/>
 	</div>
 
 	{#if error}
@@ -53,12 +61,12 @@
 		</div>
 	{/if}
 	{#if data && enabled}
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
 			{#each data.blogPosts as post}
-				<article class="flex flex-col gap-4 my-8 border shadow place-content-between">
+				<article class="border flex flex-col shadow my-8 gap-4 place-content-between">
 					<div>
 						<Image data={post.coverImage.responsiveImage} />
-						<h6 class="text-xl font-semibold hover:underline hover:opacity-50 p-2">
+						<h6 class="font-semibold text-xl p-2 hover:opacity-50 hover:underline">
 							<a rel="external" href={`https://www.datocms.com/blog/${post.slug}`}>
 								{post.title}
 							</a>
@@ -68,7 +76,7 @@
 						<StructuredText data={post.excerpt} />
 					</div>
 					<footer
-						class="inline-flex pr-2 place-items-center place-content-between gap-6 bg-gray-300"
+						class="bg-gray-300 pr-2 gap-6 inline-flex place-items-center place-content-between"
 					>
 						<div class="h-10 w-10 overflow-hidden">
 							<Image data={post.author.avatar.responsiveImage} />
@@ -76,10 +84,10 @@
 						Written by {post.author.name}
 					</footer>
 				</article>
-				{:else}
+			{:else}
 				No Posts
 			{/each}
 		</div>
 	{/if}
 </main>
-<slot/>
+<slot />
